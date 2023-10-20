4a0b2d4c98122554db7fb3dd9adf0b89124fc153
Jay <jay.180106@gmail.com>
2023-10-20 13:18:19 +0000

    added help windows

---
+++ helpWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace mahjongNEA
+{
+    /// <summary>
+    /// Interaction logic for helpWindow.xaml
+    /// </summary>
+    public partial class helpWindow : Window
+    {
+        public helpWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void collapseButton(object sender, RoutedEventArgs e)
+        {
+            leftListView.Visibility = leftListView.Visibility == Visibility.Collapsed ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
