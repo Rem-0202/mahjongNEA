642689aba999852ab32e357ee2aa6e620a793b69
Jay <jay.180106@gmail.com>
2023-10-20 13:31:07 +0000

    chnaged everything

--- Tile.xaml.cs
+++ Tile.xaml.cs
@@ -24,6 +24,7 @@ namespace mahjongNEA
         public static List<char> suitOrder = new List<char>() { 'm', 'p', 's', 'z', 'f', 'n' };
         public int rank { get; private set; }
         public char suit { get; private set; }
+        public int suitID { get; private set; }
         public string tileID { get; private set; }
         public bool terminal { get; private set; }
         public bool honour { get; private set; }
@@ -42,20 +43,23 @@ namespace mahjongNEA
             tileID = r.ToString() + s;
             terminal = (suit == 'm' || suit == 'p' || suit == 's') && (rank == 1 || rank == 9);
             honour = suit == 'z';
+            suitID = suitOrder.IndexOf(suit);
             special = terminal || honour;
             bonus = suit == 'f' || suit == 'n';
-            name.Text = tileID;
-            name.FontSize = 13;
+            BitmapImage myBitmapImage = new BitmapImage();
+            myBitmapImage.BeginInit();
+            myBitmapImage.UriSource = new Uri(@"tempImage.jpg");
+            myBitmapImage.DecodePixelWidth = 200;
+            myBitmapImage.EndInit();
+            tileImage.Source = myBitmapImage;
         }
 
         public static bool operator ==(Tile a, Tile b) => a.tileID == b.tileID;
 
         public static bool operator !=(Tile a, Tile b) => a.tileID != b.tileID;
 
-        public static bool operator >(Tile a, Tile b) => suitOrder.IndexOf(a.suit) == suitOrder.IndexOf(b.suit) ? a.rank > b.rank : suitOrder.IndexOf(a.suit) > suitOrder.IndexOf(b.suit);
+        public static bool operator >(Tile a, Tile b) => a.suitID > b.suitID || (a.suitID == b.suitID && a.rank > b.rank);
 
-        public static bool operator <(Tile a, Tile b) => suitOrder.IndexOf(a.suit) == suitOrder.IndexOf(b.suit) ? a.rank < b.rank : suitOrder.IndexOf(a.suit) < suitOrder.IndexOf(b.suit);
-        
-
+        public static bool operator <(Tile a, Tile b) => a.suitID < b.suitID || (a.suitID == b.suitID && a.rank < b.rank);
     }
 }
